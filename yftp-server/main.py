73e79408e647bcecc80df7ec43d64a14fe810323
SneakyPanda <34578192+SneakyPanda64@users.noreply.github.com>
2022-11-17 00:25:57 +0000

    Fixed requirements.txt

--- yftp-server/main.py
+++ yftp-server/main.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
         logging.FileHandler(filename=os.path.join("logs", f"{int(time.time())}.log"), encoding="utf-8"),
         logging.StreamHandler(sys.stdout)
     ])
-    #youtube.uploadVideo("V2h5IEFsZ29yaXRobXMgTWF0dGVyIGluIE1hY2hpbmUgTGVhcm5pbmc")
     ftp = multiprocessing.Process(target=ftp.initialise_connection)
     yt = multiprocessing.Process(target=youtube.autoChecker)
     ftp.start()
